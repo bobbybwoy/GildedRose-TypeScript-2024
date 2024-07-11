3d70e056e93ebfdb16171e75cafcee4a1617c9b0
Robert De Leon <robert.deleon@softwire.com>
2024-07-11 14:21:10 +0100

    Unit tests implemented

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -17,6 +17,13 @@ export class GildedRose {
     this.items = items;
   }
 
+  //increase quality(byN)
+  //decrease sellIn
+  //is Aged Brie
+  //is Backstage passes
+  //is Sulfuras
+
+
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
@@ -42,9 +49,11 @@ export class GildedRose {
           }
         }
       }
+
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
+
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
